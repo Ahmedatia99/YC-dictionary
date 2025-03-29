617b14bea70efa2946d99de93a060a0c9c5428ce
Ahmedatia99 <atyaa629@gmail.com>
2025-03-29 09:20:45 +0200

    Startup Details

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,10 @@
 import {defineQuery} from "next-sanity"
 export const STARTUP_QUERY = 
-defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt desc){
-  _id,
+defineQuery(`
+  *[_type == "startup" && defined(slug.current) 
+    && (!defined($search) || category match $search || title match $search  || author->name match $search)
+  ] | order(_createdAt desc) {
+    _id,
     title,
     views,
     slug,
@@ -12,4 +15,20 @@ defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt d
       _id, name, image, bio
     },
     category
+  }
+`)
+export const STARTUP_BY_ID_QUERY = (`*[_type == "startup" && _id == $id][0]{
+  _id,
+    title,
+    views,
+    slug,
+    description,
+    _createdAt,
+    image,
+    author ->{
+      _id, name,username, image, bio
+    },
+    category,
+    image,
+    pitch
 }`)
