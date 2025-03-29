1c5c9241a9114e0808d429ca040d74f4050716f5
Ahmedatia99 <atyaa629@gmail.com>
2025-03-29 09:20:24 +0200

    Real-time Search w/ URL Query Params

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,8 +7,9 @@ export default async function Home({
 }: {
   searchParams: Promise<{ query?: string }>;
 }) {
-  const query = (await searchParams).query;
-  const { data: posts } = await sanityFetch({ query: STARTUP_QUERY });
+  const query = (await searchParams).query
+  const params ={search : query || null}
+  const { data: posts } = await sanityFetch({ query: STARTUP_QUERY, params });
   return (
     <>
       <div className=" pink_container pattern !bg-primary">
