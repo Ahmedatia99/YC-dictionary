2afe312bf6e608c14c50e59c7a67e06c318382f3
Ahmedatia99 <atyaa629@gmail.com>
2025-04-05 15:11:23 +0200

    add Views component with Ping + some classes in style + skeleton from shadcn ui

---
+++ components/Views.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Ping from "./Ping";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+
+const Views = async ({ id }: { id: { id: string } }) => {
+  const { views: totalView } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY, { id });
+    console.log(totalView)
+  return (
+    <div className="view-container">
+      <div className="absolute -top-2 -right-2">
+        <Ping />
+      </div>
+      <div className="view-text">
+        <span className="font-black">views : {totalView} </span>
+      </div>
+    </div>
+  );
+};
+
+export default Views;
