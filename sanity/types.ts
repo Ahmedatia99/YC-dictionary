b170e1246fbff6d4bb07a3e1da82a635d04a1eb0
Ahmedatia99 <atyaa629@gmail.com>
2025-04-10 20:08:04 +0200

    add write-client to live view

--- sanity/types.ts
+++ sanity/types.ts
@@ -143,7 +143,24 @@ export type Startup = {
   description?: string;
   category?: string;
   image?: string;
-  pitch?: string;
+  pitch?: Array<{
+    children?: Array<{
+      marks?: Array<string>;
+      text?: string;
+      _type: "span";
+      _key: string;
+    }>;
+    style?: "normal" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "blockquote";
+    listItem?: "bullet" | "number";
+    markDefs?: Array<{
+      href?: string;
+      _type: "link";
+      _key: string;
+    }>;
+    level?: number;
+    _type: "block";
+    _key: string;
+  }>;
 };
 
 export type Slug = {
