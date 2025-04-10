dea34e0c703a4619d75509c983bd86fc4d1f2049
Ahmedatia99 <atyaa629@gmail.com>
2025-04-10 17:35:37 +0200

    push live api && edit in queries && pitch solve his problem

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,22 +1,26 @@
 import {defineQuery} from "next-sanity"
-export const STARTUP_QUERY = 
-defineQuery(`
-  *[_type == "startup" && defined(slug.current) 
-    && (!defined($search) || category match $search || title match $search  || author->name match $search)
-  ] | order(_createdAt desc) {
-    _id,
-    title,
-    views,
-    slug,
-    description,
-    _createdAt,
-    image,
-    author ->{
-      _id, name, image, bio
-    },
-    category
-  }
-`)
+export const STARTUP_QUERY = defineQuery(`*[
+  _type == "startup" && 
+  defined(slug.current) && 
+  (
+    !defined($search) || 
+    title match $search || 
+    category match $search || 
+    author->name match $search
+  )
+] | order(_createdAt desc) {
+  _id, 
+  title, 
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, image, bio
+  }, 
+  views,
+  description,
+  category,
+  image,
+}`);
 export const STARTUP_BY_ID_QUERY = (`*[_type == "startup" && _id == $id][0]{
   _id,
     title,
