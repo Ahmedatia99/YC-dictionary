ab740a9748ccd7378b11ff627c326b939120d379
Ahmedatia99 <atyaa629@gmail.com>
2025-04-16 21:29:58 +0200

    update next file config to use image from all places

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "t3.ftcdn.net",
-      "myglobalheart.org",
-      "cdn.sanity.io",
-      "portfolio-ahmed-atias-projects-647e742a.vercel.app",
-      "images.pexels.com"
-    ],
+    domains: [],
   },
 };
 
